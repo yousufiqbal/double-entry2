7516f2a657a871a73eb6c8a074324ff8e274b569
Yousuf Iqbal <yousufiqbalhahim@gmail.com>
2022-02-09 10:13:49 +0500

    asdf

---
+++ src/routes/accounts/[name].svelte
@@ -0,0 +1,9 @@
+<script>
+import { page } from "$app/stores";
+import { Title } from "@yousufiqbal/ui";
+import { startCase } from "lodash-es";
+
+
+</script>
+
+<Title text="{startCase($page.params.name)} Account" />
